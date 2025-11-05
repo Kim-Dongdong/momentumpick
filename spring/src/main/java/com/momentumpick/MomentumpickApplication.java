f93e8b7e26260160ab650874bdb44a8e4caee193
Kim-Dongdong <kdssos11@naver.com>
2025-11-05 17:52:48 +0900

    initial commit

---
+++ spring/src/main/java/com/momentumpick/MomentumpickApplication.java
@@ -0,0 +1,13 @@
+package com.momentumpick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomentumpickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MomentumpickApplication.class, args);
+	}
+
+}
